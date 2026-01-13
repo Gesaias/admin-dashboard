8591c087430b59acb808a7136fef98890601a9e3
Gesaias <gesaias@publica-ro.com.br>
2026-01-13 16:24:37 -0400

    configs api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,37 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Admin Dashboard - API')
+    .setDescription('API Admin Dashboard')
+    .setVersion('0.0.1')
+    .addTag('Api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/document', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
